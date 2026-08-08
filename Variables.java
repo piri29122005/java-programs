cf36fb2069f29f08892fd216861cf2e6032933e4
Priya Dharshini <piri29122005@gmail.com>
2026-08-08 11:36:43 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables{
+    public static void main(String[] args){
+        int age = 20;
+        String name = "Priya";
+        char initial = 'M';
+        float salary = 4.5f;
+         
+        System.out.println("age");
+        System.out.println("name");
+        System.out.println("initial");
+        System.out.println("salary");
+    }
+}
