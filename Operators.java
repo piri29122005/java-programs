537931b0f19b5946f503dee16ff6bb35fdfd188f
Priya Dharshini <piri29122005@gmail.com>
2026-08-09 17:38:03 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,24 @@
+// public class Operators{
+//     public static void main(String[] args){
+//         int money = 100;
+//         money += 50;
+//         money -=30;
+
+//         System.out.println(money);
+//     }
+// }/
+
+
+public class Operators{
+    public static void main(String[] args){
+        int number = 10;
+        number++;
+        number += 5;
+        number--;
+        number-=2;
+
+        System.err.println(number);
+
+
+    }
+}
