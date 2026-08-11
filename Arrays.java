1dc1c9168a2e1b48a3a68c3854ccc4e73fa11ff1
Priya Dharshini <piri29122005@gmail.com>
2026-08-11 17:09:41 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,70 @@
+// 1..// import java.util.Scanner;
+// public class Arrays{
+//     public static void main(String[] args){
+
+//         Scanner sc = new Scanner(System.in);
+
+//         int[] numbers = new int[10];
+
+//         int evenCount = 0;
+//         int oddCount = 0;
+
+//         for(int i = 0; i < 10; i++){
+//             numbers[i] = sc.nextInt();
+
+//             if(numbers[i] % 2 == 0){
+//                 evenCount++;
+//                  }
+
+//             else{
+//                 oddCount++;
+//             }
+//         }
+//            System.out.println("Enter Even numbers:" + evenCount);
+//            System.out.println("Enter Odd numbers:" + oddCount); 
+
+//     }
+// }
+
+// 2.// import java.util.Scanner;
+// public class Arrays{
+//     public static void main(String[] args){
+
+//         Scanner sc = new Scanner(System.in);
+
+//         int[] elements = new int[5];
+//         System.out.println("Enter 5 elements:");
+
+
+//         for(int i=0;i<5;i++){
+//             elements[i] = sc.nextInt();
+            
+//         }
+//         System.out.println("All elements are:");
+//         for(int j=0;j<5;j++){
+//             System.out.println(elements[j]);
+
+//         }
+
+//     }
+// }
+
+// Sum of elements in arrays
+
+// import java.util.Scanner;
+// public class Arrays{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         int[] numbers = new int[5];
+//         int sum = 0;
+
+//         for(int i=0; i<5; i++){
+//             numbers[i] = sc.nextInt();
+//         }
+//         for(int j=0;j<5;j++){
+//          sum = sum + numbers[j];
+//         }
+//         System.out.println("Sum:" + sum);
+//     }
+// }
