f9ea6e48fe87161b9a0e6c69282d7ff92d69f3d5
Priya Dharshini <piri29122005@gmail.com>
2026-08-12 10:53:04 +0530

    Add files via upload

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,43 @@
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean isSorted = true;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        if (isSorted) {
+            System.out.println("The Array is Sorted");
+        } else {
+            System.out.println("The Array is Not Sorted");
+        }
+
+        sc.close();
+    }
+}
+
+
+
 // 1..// import java.util.Scanner;
 // public class Arrays{
 //     public static void main(String[] args){
