69053475638f7dc43cc9afc0695c9d7a1d171638
Priya Dharshini <piri29122005@gmail.com>
2026-08-12 16:05:30 +0530

    Add files via upload

---
+++ Condition.java
@@ -0,0 +1,376 @@
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+        
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter First number: ");
+//         int a = sc.nextInt();
+
+//         System.out.println("Enter Second number: ");
+//         int b = sc.nextInt();
+
+//         System.out.println("Enter third number: ");
+//         int c = sc.nextInt();
+
+//         if(a>=b && a>=c){
+//             System.out.println("Largest number :" + a);
+//         }
+
+//         else if(b>=a && b>=c){
+//             System.out.println("Largest number :" + b);
+//         }
+//         else{
+//             System.out.println("Largest number: " + c);
+//         }
+
+//         sc.close();
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+        
+//         Scanner sc = new Scanner(System.in);
+        
+//         System.out.println("Enter the Number: ");
+//         int num = sc.nextInt();
+
+//         if(num >= 0){
+//             System.out.println("Positive");
+//             }
+//         else if(num < 0 ){
+//             System.out.println("negative");
+//         }
+//         else {
+//             System.out.println("Zero"); 
+//                }
+//                sc.close();
+//     }
+// }
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the number: ");
+//         int num = sc.nextInt();
+
+//         if(num % 2 == 0){
+//             System.out.println("Even");
+//         }
+//         else{
+//             System.out.println("Odd");
+//         }
+
+//         sc.close();
+//     }
+// }
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter the number: ");
+//         int mark = sc.nextInt();
+
+//         if(mark >= 50){
+//             System.out.println("Pass");
+//         }
+//         else{
+//             System.out.println("Fail");
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println(" Enter First number: ");
+//         int a = sc.nextInt();
+
+//         System.out.println(" Enter Second number: ");
+//         int b = sc.nextInt();
+
+//       if(a>=b){
+//         System.out.println("Largest number:" + a);
+
+//       }
+//       else{
+//         System.out.println("Largest number:" + b);
+//       }
+//       sc.close();
+//     }
+    
+// }
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println(" Enter the age: ");
+//         int age = sc.nextInt();
+
+        
+
+//       if(age >= 18){
+//         System.out.println("Eligible");
+
+//       }
+//       else{
+//         System.out.println("Not Eligible");
+//       }
+//       sc.close();
+//     }
+    
+// }
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter First Number: ");
+//         int a = sc.nextInt();
+        
+//         System.out.println("Enter Second Number: ");
+//         int b = sc.nextInt();
+
+//         System.out.println("Enter third Number: ");
+//         int c = sc.nextInt();
+        
+//         if(a>=b && a>=c){
+//         System.out.println("Largest Number:" + a);
+
+//       }
+//       else if(b>=a && b>=c){
+//         System.out.println("Largest Number:" + b);
+//       }
+//       else{
+//         System.out.println("Largest Number:" + c);
+//       }
+//       sc.close();
+//     }
+// }
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter the mark: ");
+//         int mark = sc.nextInt();
+        
+//         if(mark>=90){
+//         System.out.println("Grade A");
+
+//       }
+//       else if(mark >= 80){
+//         System.out.println("Grade B");
+//       }
+//       else if (mark >= 70){
+//         System.out.println("Grade C");
+
+//       }
+//        else if (mark >= 60){
+//         System.out.println("Grade D");
+
+//       }
+//        else if (mark >= 50){
+//         System.out.println("Grade E");
+
+//       }
+//       else{
+//         System.out.println("Fail");
+//       }
+//       sc.close();
+//     }
+// }
+   
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter the year: ");
+//         int year = sc.nextInt();
+
+//         if(year % 4 == 0){
+//             System.out.println("It's a Leap year");
+//         }
+//         else{
+//             System.out.println("Not a Leap year");
+//         }
+//     }
+    
+// }
+
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+        
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter first number :");
+//         int a = sc.nextInt();
+
+//         System.out.println("Enter Second number :");
+//         int b = sc.nextInt();
+
+//         char operator = sc.next().charAt(0);
+//         switch(operator){
+//             case '+' :
+//                 System.out.println("Add");
+//                 break;
+
+//              case '-' :
+//                 System.out.println("Sub");
+//                 break;
+            
+//              case '*' :
+//                 System.out.println("Multi");
+//                 break;
+//              case '%' :
+//                 System.out.println("Modules");
+//                 break;
+//              case '/' :
+//                 System.out.println("Div");
+//                 break;
+//             default:
+//                 System.out.println("Invalid");
+            
+            
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+//          Scanner sc = new Scanner(System.in);
+        
+//         System.out.println("Enter units:");
+//         int units = sc.nextInt();
+
+//         if(units <= 100){
+//             System.out.println("Rs 2");
+//         }
+//         else if(units <= 200){
+//             System.out.println("Rs 3");
+//         }
+//         else if(units <= 300){
+//             System.out.println("Rs 5");
+//         }
+//         else {
+//             System.out.println("Rs 7");
+//         }
+//         sc.close();
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class Condition{
+//     public static void main(String[] args) {
+         
+//          Scanner sc = new Scanner(System.in);
+
+//          System.out.println("Enter your balance:");
+//          int balance = sc.nextInt();
+//          System.out.println("Enter Withdrawal Amount:");
+//          int amount = sc.nextInt();
+//          System.out.println("Enter you PIN:");
+//          int PIN = sc.nextInt();
+
+//          if(PIN == 1234){
+//             if(amount > 0){
+//                 if(amount <= balance){
+//                     System.out.println("Withdrawal");
+//                 }
+//                 else{
+//                     System.out.println("insufficient balance");
+//                 }
+                
+//             }
+//             else{
+//                 System.out.println("Invalid amount");
+//             }
+
+//          }
+//          else{
+//             System.out.println("Invalid PIN");
+//          }
+//     }
+// }
+
+
+// import java.util.Scanner;
+
+// public class Condition {
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter Tamil mark: ");
+//         int tamil = sc.nextInt();
+
+//         System.out.print("Enter English mark: ");
+//         int english = sc.nextInt();
+
+//         System.out.print("Enter Maths mark: ");
+//         int maths = sc.nextInt();
+
+//         System.out.print("Enter Science mark: ");
+//         int science = sc.nextInt();
+
+//         System.out.print("Enter Computer mark: ");
+//         int computer = sc.nextInt();
+
+        
+//         int total = tamil + english + maths + science + computer;
+
+//         double average = total / 5.0;
+
+//         System.out.println("Total: " + total);
+//         System.out.println("Average: " + average);
+
+        
+//         if (tamil < 35 || english < 35 || maths < 35 ||
+//             science < 35 || computer < 35) {
+
+//             System.out.println("Result: Fail");
+
+//         } 
+//         else {
+
+//             System.out.println("Result: Pass");
+
+//             if (average >= 90) {
+//                 System.out.println("Grade: A");
+//             }
+//             else if (average >= 75) {
+//                 System.out.println("Grade: B");
+//             }
+//             else if (average >= 60) {
+//                 System.out.println("Grade: C");
+//             }
+//             else if (average >= 50) {
+//                 System.out.println("Grade: D");
+//             }
+//             else {
+//                 System.out.println("Grade: F");
+//             }
+//         }
+
+//         sc.close();
+//     }
+// }
+
